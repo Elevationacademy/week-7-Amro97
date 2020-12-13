8232cd3699743c2dc18ad818cfe565061e584601
Amro97 <57412900+Amro97@users.noreply.github.com>
2020-12-13 16:54:01 +0200

    Algos

---
+++ Algo/main.js
@@ -0,0 +1,32 @@
+//Ex1
+const printStars = (num) => {
+    for (let i = 1; i <= num; i++)
+        for (let j = 1; j <= i; j++)
+            console.log('*')
+}
+
+//Ex2
+const printBackwardsStars = (num) => {
+    let j
+    while (num) {
+        for (j = num; j > 0; j--) {
+            console.log('*')
+        }
+        num--
+    }
+}
+
+printStars(5)
+console.log('Done')
+printBackwardsStars(5)
+console.log('Done')
+
+//Ex3
+const printStarSeries = (num, count) => {
+    for (let i = 0; i < count; i++) {
+        printStars(num)
+        printBackwardsStars(num - 1)
+    }
+}
+
+printStarSeries(5, 3)
